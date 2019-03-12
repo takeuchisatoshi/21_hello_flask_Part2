1557845db06288699be30ec6799f1742d15e8bc6
takeuchisatoshi <satsitt3@gmail.com>
2019-03-12 14:03:20 +0900

    render templatesでサイコロ

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +8,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", message="Hi! hachimantai!")
+
+
+@app.route("/", methods=["GET"])
+def dice():
+    result = random.randint(1, 6)
+    return render_template("dice.html", result=result)
 
 
 if __name__ == '__main__':
